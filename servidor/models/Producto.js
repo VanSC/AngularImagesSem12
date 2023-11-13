61e1a8d36151d2b5cb20e7a79cd0e048d0dfec1f
VanSC <vanessa.choque@tecsup.edu.pe>
2023-11-13 11:55:55 -0500

    first commit

---
+++ servidor/models/Producto.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const ProductoSchema = new Schema({
+    
+    producto: {
+        type: String,
+        require: true
+    },
+    categoria: {
+        type: String,
+        require: true
+    },
+    ubicacion: {
+        type: String,
+        require: true
+    },
+    precio: {
+        type: Number,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = model('Producto', ProductoSchema)
